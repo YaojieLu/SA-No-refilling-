70428c77379d83d43de93d320e2a6ac822b9cdeb
Yaojie Lu <tanisraistlin@gmail.com>
2017-03-22 15:54:27 +1100

    1

--- Sensitivity Analysis/pxmin - P50.r
+++ Sensitivity Analysis/pxmin - P50.r
@@ -4,8 +4,6 @@ options(digits=20)
 
 # Parameterization
 ca <- 400
-k <- 0.05
-MAP <- 1825
 LAI <- 1
 Vcmax <- 50
 cp <- 30
@@ -23,14 +21,13 @@ c <- 2.64
 #d <- 3.54
 h <- l*a*LAI/nZ*p
 h2 <- l*LAI/nZ*p/1000
-gamma <- 1/((MAP/365/k)/1000)*nZ
 
-df <- data.frame(pxmin=rep(0, 10), P50=rep(0, 10))
+df <- data.frame(P50=rep(0, 10), pxmin=rep(0, 10))
 wL <- c(0.237391588872621, 0.199489329739771, 0.179524342792369, 0.166376542776724, 0.156779197039201, 0.149332299107777, 0.143312182451349, 0.138299320937904, 0.134030585048342, 0.130331075377533)
-
+dSA <- rep(seq(1, 10, by=1), 1)
 for(i in 1:10){
-  d <- i
-  df[i, 1] <- psf(wL[i])
-  df[i, 2] <- Psi50fd(d)
+  d <- dSA[i]
+  df[i, 1] <- Psi50fd(d)
+  df[i, 2] <- psf(wL[i])
 }
 df
